0d89509b5f6af13d5a79e43493c48dfab217474c
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2024-07-26 23:38:18 +0100

    Fetch in Parallel

--- manipulate-svg.js
+++ manipulate-svg.js
@@ -49,24 +49,12 @@ async function fetchCoordinatesAndAddCircles(svgContainer) {
     params = new URLSearchParams(url.search);
     // Get the value of the 'cmdr' parameter
     const cmdr = params.get('cmdr');
-    var cmdr_system = params.get('system');
 
     if (!cmdr) {
         console.error('No cmdr parameter found in the URL');
         return;
     }
 
-    if (!cmdr_system) {
-        cmdr_system = "Sol"
-        var cmdr_coords = [0, 0, 0];
-    } else {
-        var cmdr_coords = await fetchCoordinates(cmdr_system);
-    }
-
-    document.getElementById('search-box').value = cmdr;
-    console.log("did you set it?")
-
-    // Fetch the list of entryid and hud_category from the API
     const apiUrl = `https://us-central1-canonn-api-236217.cloudfunctions.net/query/missing/codex?cmdr=${cmdr}`;
 
     try {
@@ -76,88 +64,94 @@ async function fetchCoordinatesAndAddCircles(svgContainer) {
         }
         const data = await apiResponse.json();
 
-        if (!Array.isArray(data)) {
-            throw new Error('API response is not an array');
+        // we could filter some but no need
+        const filteredItems = Object.values(data)
+
+        // Process each item asynchronously
+        for (const item of filteredItems) {
+            fetchAndRenderItem(svgContainer, item);
         }
 
-        for (const item of data) {
-            const { entryid, hud_category, english_name } = item;
-
-            if (!entryid || !hud_category || !english_name) {
-                console.warn('Invalid item in API response', item);
-                continue;
-            }
-
-            const coordinatesUrl = `https://storage.googleapis.com/canonn-downloads/dumpr/${hud_category}/${entryid}.csv`;
-
-            try {
-                const response = await fetch(coordinatesUrl);
-                if (!response.ok) {
-                    throw new Error(`CSV request failed with status ${response.status}`);
-                }
-                const csvText = await response.text();
-                const svgElement = svgContainer.querySelector('svg');
-
-                if (svgElement) {
-                    // Split CSV text into lines
-                    const lines = csvText.trim().split('\n');
-                    const circleGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-                    svgElement.append(circleGroup); // Add the group as the first child of the SVG
-
-                    // Process each line (assuming no header and x, y are in fields 2 and 3)
-                    let index = 0;
-                    const batchSize = 1000; // Number of circles to add in each batch
-                    const intervalId = setInterval(() => {
-                        for (let i = 0; i < batchSize && index < lines.length; i++) {
-                            const line = lines[index];
-                            const fields = line.split(',');
-                            if (fields.length >= 3) {
-                                const x = parseFloat(fields[1]);
-                                const y = parseFloat(fields[3]);
-                                const title = fields[0] + '<br>' + english_name
-                                const cRegion = fields[6]
-                                const bRegion = fields[7]
-
-                                var regionElement = svgElement.getElementById("Region_" + cRegion.padStart(2, '0'));
-                                // Check if the element exists
-                                if (regionElement) {
-                                    // Set the fill-opacity to 50%
-                                    regionElement.style.fillOpacity = "0.5";
-                                }
-
-                                if (!isNaN(x) && !isNaN(y)) {
-                                    tx = ((x + 49985) * 83 / 4096)
-                                    ty = ((y + 24105) * 83 / 4096)
-
-                                    addCircleToSVG(svgElement, circleGroup, tx, 2048 - ty, title, fields[0], entryid, hud_category);
-                                }
-                            }
-                            index++;
-                        }
-                        if (index >= lines.length) {
-                            clearInterval(intervalId); // Stop interval when all circles are added
-                        }
-                    }, 0); // Interval of 0ms (no delay)
-
-                    var paths = svgElement.querySelectorAll('.borderlines');
-                    paths.forEach(function (path) {
-                        svgElement.appendChild(path); // Move each text element to the end of the SVG
-                        path.style.pointerEvents = "none";
-                    });
-
-                    var texts = svgElement.querySelectorAll('text');
-                    texts.forEach(function (text) {
-                        svgElement.appendChild(text); // Move each text element to the end of the SVG
-                        text.style.pointerEvents = "none";
-                    });
-                }
-            } catch (error) {
-                console.error(`Error fetching CSV for entryid ${entryid} and hud_category ${hud_category}:`, error);
-            }
-        }
     } catch (error) {
         console.error('Error fetching data from API:', error);
     }
+}
+
+async function fetchAndRenderItem(svgContainer, item) {
+    const { entryid, hud_category } = item;
+    const coordinatesUrl = `https://storage.googleapis.com/canonn-downloads/dumpr/${hud_category}/${entryid}.csv`;
+
+    try {
+        const response = await fetch(coordinatesUrl);
+        if (!response.ok) {
+            throw new Error(`CSV request failed with status ${response.status}`);
+        }
+        const csvText = await response.text();
+        await renderData(svgContainer, item, csvText);
+
+    } catch (error) {
+        console.error(`Error fetching CSV for entryid ${entryid} and hud_category ${hud_category}:`, error);
+    }
+}
+
+async function renderData(svgContainer, item, csvText) {
+    const svgElement = svgContainer.querySelector('svg');
+
+    if (!svgElement) return;
+
+    const { entryid, hud_category, english_name } = item;
+
+    // Split CSV text into lines
+    const lines = csvText.trim().split('\n');
+    const circleGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    svgElement.append(circleGroup); // Add the group as the first child of the SVG
+
+    // Process each line (assuming no header and x, y are in fields 2 and 3)
+    let index = 0;
+    const batchSize = 1000; // Number of circles to add in each batch
+    const intervalId = setInterval(() => {
+        for (let i = 0; i < batchSize && index < lines.length; i++) {
+            const line = lines[index];
+            const fields = line.split(',');
+            if (fields.length >= 3) {
+                const x = parseFloat(fields[1]);
+                const y = parseFloat(fields[3]);
+                const title = fields[0] + '<br>' + english_name;
+                const cRegion = fields[6];
+                const bRegion = fields[7];
+
+                var regionElement = svgElement.getElementById("Region_" + cRegion.padStart(2, '0'));
+                // Check if the element exists
+                if (regionElement) {
+                    // Set the fill-opacity to 50%
+                    regionElement.style.fillOpacity = "0.5";
+                }
+
+                if (!isNaN(x) && !isNaN(y)) {
+                    const tx = ((x + 49985) * 83 / 4096);
+                    const ty = ((y + 24105) * 83 / 4096);
+
+                    addCircleToSVG(svgElement, circleGroup, tx, 2048 - ty, title, fields[0], entryid, hud_category);
+                }
+            }
+            index++;
+        }
+        if (index >= lines.length) {
+            clearInterval(intervalId); // Stop interval when all circles are added
+        }
+    }, 0); // Interval of 0ms (no delay)
+
+    var paths = svgElement.querySelectorAll('.borderlines');
+    paths.forEach(function (path) {
+        svgElement.appendChild(path); // Move each text element to the end of the SVG
+        path.style.pointerEvents = "none";
+    });
+
+    var texts = svgElement.querySelectorAll('text');
+    texts.forEach(function (text) {
+        svgElement.appendChild(text); // Move each text element to the end of the SVG
+        text.style.pointerEvents = "none";
+    });
 }
 
 
